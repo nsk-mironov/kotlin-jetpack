9aff661c4b9ddb74bd54eb1c1885fb215a0340e0
Vladimir Mironov <nsk.mironov@gmail.com>
2015-08-11 23:02:34 +0300

    Initial project structure

---
+++ bender-sample/src/main/kotlin/com/github/vmironov/bender/SampleActivity.kt
@@ -0,0 +1,12 @@
+package com.github.vmironov.bender
+
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+public class SampleActivity() : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sample)
+    }
+}
