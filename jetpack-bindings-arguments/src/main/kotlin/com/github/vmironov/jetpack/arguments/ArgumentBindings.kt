818eeef35d6fb695d1e95f774509c1aba2fb2d66
Vladimir Mironov <nsk.mironov@gmail.com>
2015-09-29 22:11:57 +0300

    Introduce ArgumentsBindings

--- jetpack-bindings-arguments/src/main/kotlin/com/github/vmironov/jetpack/arguments/ArgumentBindings.kt
+++ jetpack-bindings-arguments/src/main/kotlin/com/github/vmironov/jetpack/arguments/ArgumentBindings.kt
@@ -9,12 +9,38 @@ public interface ArgumentsAware {
   public val arguments: Bundle?
 }
 
-private class ArgumentsVal<T, V>(private val source: Any, private val initializer: (Bundle) -> V) : LazyVal<T, V>({ desc, property ->
-  initializer(when (source) {
-    is Activity -> source.intent.extras
-    is Fragment -> source.arguments
-    is android.support.v4.app.Fragment -> source.arguments
-    is ArgumentsAware -> source.arguments
+internal class ArgumentsVal<T, V>(
+    private val source: Any,
+    private val name: String?,
+    private val default: V?,
+    private val initializer: (String, Bundle) -> V?
+) : LazyVal<T, V>({ desc, property ->
+  val value = initializer(name ?: property.name, when (source) {
+    is Activity -> source.intent.extras ?: Bundle.EMPTY
+    is Fragment -> source.arguments ?: Bundle.EMPTY
+    is android.support.v4.app.Fragment -> source.arguments ?: Bundle.EMPTY
+    is ArgumentsAware -> source.arguments ?: Bundle.EMPTY
     else -> throw IllegalArgumentException("Unable to find arguments on type ${source.javaClass.simpleName}")
   })
+
+  if (value == null && default == null) {
+    throw IllegalArgumentException("Key ${name ?: property.name} is missed")
+  }
+
+  value ?: default!!
+})
+
+internal class OptionalArgumentsVal<T, V>(
+    private val source: Any,
+    private val name: String?,
+    private val default: V?,
+    private val initializer: (String, Bundle) -> V?
+) : LazyVal<T, V?>({ desc, property ->
+  initializer(name ?: property.name, when (source) {
+    is Activity -> source.intent.extras ?: Bundle.EMPTY
+    is Fragment -> source.arguments ?: Bundle.EMPTY
+    is android.support.v4.app.Fragment -> source.arguments ?: Bundle.EMPTY
+    is ArgumentsAware -> source.arguments ?: Bundle.EMPTY
+    else -> throw IllegalArgumentException("Unable to find arguments on type ${source.javaClass.simpleName}")
+  }) ?: default
 })
