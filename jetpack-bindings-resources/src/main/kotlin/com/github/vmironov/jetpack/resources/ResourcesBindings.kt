cda826a22db6d64a3d646a624115adc58a4f3594
Vladimir Mironov <nsk.mironov@gmail.com>
2015-10-11 16:31:44 +0300

    Refactor ResourcesVal a bit

--- jetpack-bindings-resources/src/main/kotlin/com/github/vmironov/jetpack/resources/ResourcesBindings.kt
+++ jetpack-bindings-resources/src/main/kotlin/com/github/vmironov/jetpack/resources/ResourcesBindings.kt
@@ -9,6 +9,10 @@ import android.graphics.drawable.Drawable
 import android.view.View
 import kotlin.properties.ReadOnlyProperty
 
+public inline fun <reified T : Any> Any.bindResource(id: Int): ReadOnlyProperty<Any, T> {
+  return ResourcesVal(T::class.java, this, id)
+}
+
 public interface ResourcesAware {
   public companion object {
     public operator fun invoke(factory: () -> Resources): ResourcesAware = object : ResourcesAware {
@@ -21,124 +25,75 @@ public interface ResourcesAware {
   public val resources: Resources
 }
 
-public inline fun <reified T : Any> Any.bindResource(id: Int): ReadOnlyProperty<Any, T> {
-  return ResourcesVal(T::class.java, this, id)
-}
-
-@Suppress("UNCHECKED_CAST")
+@Suppress("UNCHECKED_CAST", "USELESS_CAST")
 public class ResourcesVal<T : Any, V : Any>(
     private val clazz: Class<V>,
     private val source: Any,
     private val id: Int
-) : LazyVal<T, V>({ desc, property ->
-  val resources = when {
-    source is ResourcesAware -> source.resources
-    source is Context -> source.resources
-    source is Fragment -> source.activity.resources
-    source is Resources -> source as Resources
-    source is View -> source.resources
-    SupportHelper.isFragment(source) -> SupportFragmentHelper.getResources(source)
-    SupportHelper.isHolder(source) -> SupportRecyclerHelper.getResources(source)
-    source is Dialog -> source.context.resources
-    else -> throw IllegalArgumentException("Unable to find resources on type ${source.javaClass.simpleName}")
-  }
-
-  fun unsupported(id: Int, clazz: Class<*>): UnsupportedOperationException {
-    return UnsupportedOperationException("Unsupported resource (name = \"${resources.getResourceName(id)}\", type = \"${clazz}\")")
-  }
-
-  @Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN", "DEPRECATED_SYMBOL_WITH_MESSAGE")
-  val value: Any = when (resources.getResourceTypeName(id)) {
-    "drawable" -> when {
-      Drawable::class.java.isAssignableFrom(clazz) -> {
-        resources.getDrawable(id)
-      }
-
-      else -> throw unsupported(id, clazz)
-    }
-
-    "bool" -> when {
-      clazz === kotlin.Boolean::class.java, clazz === java.lang.Boolean::class.java -> {
-        resources.getBoolean(id)
-      }
-
-      else -> throw unsupported(id, clazz)
-    }
-
-    "integer" -> when {
-      clazz === kotlin.Int::class.java, clazz === java.lang.Integer::class.java -> {
-        resources.getInteger(id)
-      }
-
-      else -> throw unsupported(id, clazz)
-    }
-
-    "color" -> when {
-      clazz === kotlin.Int::class.java, clazz === java.lang.Integer::class.java -> {
-        resources.getColor(id)
-      }
-
-      clazz === ColorStateList::class.java -> {
-        resources.getColorStateList(id)
-      }
-
-      else -> throw unsupported(id, clazz)
-    }
-
-    "dimen" -> when {
-      clazz === kotlin.Float::class.java, clazz === java.lang.Float::class.java -> {
-        resources.getDimension(id)
-      }
-
-      clazz === kotlin.Int::class.java, clazz === java.lang.Integer::class.java -> {
-        resources.getDimensionPixelSize(id)
-      }
-
-      else -> throw unsupported(id, clazz)
-    }
-
-    "string" -> when {
-      clazz === kotlin.String::class.java, clazz === java.lang.String::class.java -> {
-        resources.getString(id)
-      }
-
-      clazz === kotlin.CharSequence::class.java, clazz === java.lang.CharSequence::class.java -> {
-        resources.getText(id)
-      }
-
-      else -> throw unsupported(id, clazz)
-    }
-
-    "array" -> when {
-      clazz === IntArray::class.java, clazz === Array<kotlin.Int>::class.java, clazz === Array<java.lang.Integer>::class.java -> {
-        resources.getIntArray(id)
-      }
-
-      clazz === Array<kotlin.CharSequence>::class.java, clazz === Array<java.lang.CharSequence>::class.java -> {
-        resources.getTextArray(id)
-      }
-
-      clazz === Array<kotlin.String>::class.java, clazz === Array<java.lang.String>::class.java -> {
-        resources.getStringArray(id)
-      }
-
-      else -> throw unsupported(id, clazz)
-    }
-
-    else -> throw unsupported(id, clazz)
-  }
-
-  value as V
-})
-
-private open class LazyVal<T, V>(private val initializer : (T, PropertyMetadata) -> V) : ReadOnlyProperty<T, V> {
+) : ReadOnlyProperty<T, V> {
   private var value: Any? = Unit
 
   override operator fun get(thisRef: T, property: PropertyMetadata): V {
     if (value === Unit) {
-      value = initializer(thisRef, property)
+      value = onLazyGetValue(when {
+        source is ResourcesAware -> source.resources
+        source is Context -> source.resources
+        source is Fragment -> source.activity.resources
+        source is Resources -> source as Resources
+        source is View -> source.resources
+        SupportHelper.isFragment(source) -> SupportFragmentHelper.getResources(source)
+        SupportHelper.isHolder(source) -> SupportRecyclerHelper.getResources(source)
+        source is Dialog -> source.context.resources
+        else -> throw IllegalArgumentException("Unable to find resources on type ${source.javaClass.simpleName}")
+      })
     }
-    @Suppress("UNCHECKED_CAST")
+
     return value as V
+  }
+
+  @Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN", "DEPRECATED_SYMBOL_WITH_MESSAGE", "IMPLICIT_CAST_TO_UNIT_OR_ANY")
+  private fun onLazyGetValue(resources: Resources): V {
+    val type = resources.getResourceTypeName(id)
+    val typed = { desiredType: String, desiredClass: Class<*> ->
+      desiredType == type && desiredClass.isAssignableFrom(clazz)
+    }
+
+    return when {
+      typed("drawable", Drawable::class.java) -> resources.getDrawable(id)
+
+      typed("bool", kotlin.Boolean::class.java) -> resources.getBoolean(id)
+      typed("bool", java.lang.Boolean::class.java) -> resources.getBoolean(id)
+
+      typed("integer", kotlin.Int::class.java) -> resources.getInteger(id)
+      typed("integer", java.lang.Integer::class.java) -> resources.getInteger(id)
+
+      typed("color", kotlin.Int::class.java) -> resources.getColor(id)
+      typed("color", java.lang.Integer::class.java) -> resources.getColor(id)
+      typed("color", ColorStateList::class.java) -> resources.getColorStateList(id)
+
+      typed("dimen", kotlin.Float::class.java) -> resources.getDimension(id)
+      typed("dimen", java.lang.Float::class.java) -> resources.getDimension(id)
+
+      typed("dimen", kotlin.Int::class.java) -> resources.getDimension(id)
+      typed("dimen", java.lang.Integer::class.java) -> resources.getDimension(id)
+
+      typed("string", kotlin.String::class.java) -> resources.getString(id)
+      typed("string", java.lang.String::class.java) -> resources.getString(id)
+
+      typed("string", kotlin.CharSequence::class.java) -> resources.getText(id)
+      typed("string", java.lang.CharSequence::class.java) -> resources.getText(id)
+
+      typed("array", IntArray::class.java) -> resources.getIntArray(id)
+      typed("array", Array<kotlin.Int>::class.java) -> resources.getIntArray(id)
+      typed("array", Array<java.lang.Integer>::class.java) -> resources.getIntArray(id)
+
+      typed("array", Array<kotlin.String>::class.java) -> resources.getStringArray(id)
+      typed("array", Array<java.lang.String>::class.java) -> resources.getStringArray(id)
+
+      typed("array", Array<kotlin.CharSequence>::class.java) -> resources.getTextArray(id)
+      typed("array", Array<java.lang.CharSequence>::class.java) -> resources.getTextArray(id)
+
+      else -> throw UnsupportedOperationException("Unsupported resource (name = \"${resources.getResourceName(id)}\", type = \"${clazz.canonicalName}\")")
+    } as V
   }
 }
