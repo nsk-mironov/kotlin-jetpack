ee9bb1c9d4a488f5a43356c69ed4e61d3609310c
Vladimir Mironov <nsk.mironov@gmail.com>
2015-10-11 22:52:33 +0300

    Introduce arguments adapter. Closes #7 and #11

--- jetpack-bindings-arguments/src/main/kotlin/com/github/vmironov/jetpack/arguments/ArgumentsBindings.kt
+++ jetpack-bindings-arguments/src/main/kotlin/com/github/vmironov/jetpack/arguments/ArgumentsBindings.kt
@@ -5,7 +5,6 @@ import android.app.Fragment
 import android.content.Intent
 import android.os.Bundle
 import android.os.Parcelable
-import android.text.TextUtils
 import java.io.Serializable
 import kotlin.properties.ReadWriteProperty
 
@@ -13,22 +12,23 @@ public interface ArgumentsAware {
   public var arguments: Bundle?
 }
 
-public inline fun <reified T : Any> Any.bindArgument(name: String? = null, default: T? = null): ReadWriteProperty<Any, T> {
-  return ArgumentsVar(T::class.java, this, name, default)
+public inline fun <reified T : Any> Any.bindArgument(name: String? = null, default: T? = null, adapter: Adapter<T>? = null): ReadWriteProperty<Any, T> {
+  return ArgumentsVar(T::class.java, adapter, this, name, default)
 }
 
-public inline fun <reified T : Any> Any.bindOptionalArgument(name: String? = null, default: T? = null): ReadWriteProperty<Any, T?> {
-  return OptionalArgumentsVar(T::class.java, this, name, default)
+public inline fun <reified T : Any> Any.bindOptionalArgument(name: String? = null, default: T? = null, adapter: Adapter<T>? = null): ReadWriteProperty<Any, T?> {
+  return OptionalArgumentsVar(T::class.java, adapter, this, name, default)
 }
 
 @Suppress("BASE_WITH_NULLABLE_UPPER_BOUND")
 public class ArgumentsVar<T, V>(
     private val clazz: Class<V>,
+    private val adapter: Adapter<V>?,
     private val source: Any,
     private val name: String?,
     private val default: V?
 ) : ReadWriteProperty<T, V> {
-  private val delegate = ArgumentsVarDelegate<T, V>(clazz, source, name, default)
+  private val delegate = ArgumentsVarDelegate<T, V>(adapter ?: createTypeAdapterFor(clazz), source, name, default)
 
   override operator fun get(thisRef: T, property: PropertyMetadata): V {
     return delegate.get(thisRef, property) ?: throw IllegalArgumentException("Key ${name ?: property.name} is missed")
@@ -42,11 +42,12 @@ public class ArgumentsVar<T, V>(
 @Suppress("BASE_WITH_NULLABLE_UPPER_BOUND")
 public class OptionalArgumentsVar<T, V>(
     private val clazz: Class<V>,
+    private val adapter: Adapter<V>?,
     private val source: Any,
     private val name: String?,
     private val default: V?
 ) : ReadWriteProperty<T, V?> {
-  private val delegate = ArgumentsVarDelegate<T, V>(clazz, source, name, default)
+  private val delegate = ArgumentsVarDelegate<T, V>(adapter ?: createTypeAdapterFor(clazz), source, name, default)
 
   override operator fun get(thisRef: T, property: PropertyMetadata): V? {
     return delegate.get(thisRef, property)
@@ -59,7 +60,7 @@ public class OptionalArgumentsVar<T, V>(
 
 @Suppress("BASE_WITH_NULLABLE_UPPER_BOUND")
 private class ArgumentsVarDelegate<T, V>(
-    private val clazz: Class<V>,
+    private val adapter: Adapter<V>,
     private val source: Any,
     private val name: String?,
     private val default: V?
@@ -70,10 +71,10 @@ private class ArgumentsVarDelegate<T, V>(
   override operator fun get(thisRef: T, property: PropertyMetadata): V? {
     if (dirty) {
       val extra = name ?: property.name
-      val bundle = onGetArgumentsFromSource(source) ?: Bundle.EMPTY
+      val bundle = getArgumentsFromSource(source) ?: Bundle.EMPTY
 
       value = if (bundle.containsKey(extra)) {
-        onGetExtraFromBundle(bundle, extra, clazz)
+        adapter.get(bundle, extra)
       } else {
         null
       }
@@ -86,16 +87,16 @@ private class ArgumentsVarDelegate<T, V>(
   }
 
   override operator fun set(thisRef: T, property: PropertyMetadata, value: V?) {
-    val bundle = onGetArgumentsFromSource(source)
+    val bundle = getArgumentsFromSource(source)
     val target = bundle ?: Bundle()
     val extra = name ?: property.name
 
     if (bundle == null) {
-      onSetArgumentsToSource(source, target)
+      setArgumentsToSource(source, target)
     }
 
     if (value != null) {
-      onSetExtraToBundle(target, extra, clazz, value)
+      adapter.set(target, extra, value)
     } else {
       target.remove(extra)
     }
@@ -103,67 +104,7 @@ private class ArgumentsVarDelegate<T, V>(
     dirty = true
   }
 
-  @Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
-  private fun <T> onGetExtraFromBundle(bundle: Bundle, extra: String, clazz: Class<T>): Any? {
-    return when {
-      clazz == kotlin.Boolean::class.java -> bundle.getBoolean(extra)
-      clazz == kotlin.Double::class.java -> bundle.getDouble(extra)
-      clazz == kotlin.Int::class.java -> bundle.getInt(extra)
-      clazz == kotlin.Long::class.java -> bundle.getLong(extra)
-      clazz == kotlin.String::class.java -> bundle.getString(extra)
-      clazz == kotlin.CharSequence::class.java -> bundle.getCharSequence(extra)
-      clazz == kotlin.Float::class.java -> bundle.getFloat(extra)
-
-      clazz == java.lang.Boolean::class.java -> bundle.getBoolean(extra)
-      clazz == java.lang.Double::class.java -> bundle.getDouble(extra)
-      clazz == java.lang.Integer::class.java -> bundle.getInt(extra)
-      clazz == java.lang.Long::class.java -> bundle.getLong(extra)
-      clazz == java.lang.String::class.java -> bundle.getString(extra)
-      clazz == java.lang.CharSequence::class.java -> bundle.getCharSequence(extra)
-      clazz == java.lang.Float::class.java -> bundle.getFloat(extra)
-
-      clazz == Parcelable::class.java -> bundle.getParcelable(extra)
-      clazz == Serializable::class.java -> bundle.getSerializable(extra)
-
-      kotlin.Enum::class.java.isAssignableFrom(clazz) -> bundle.getEnum(extra, clazz)
-      java.lang.Enum::class.java.isAssignableFrom(clazz) -> bundle.getEnum(extra, clazz)
-
-      else -> throw UnsupportedOperationException("Type ${clazz.name} is not supported")
-    }
-  }
-
-  @Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
-  private fun <T> onSetExtraToBundle(bundle: Bundle, extra: String, clazz: Class<T>, value: T) {
-    when {
-      clazz == kotlin.Boolean::class.java -> bundle.putBoolean(extra, value as Boolean)
-      clazz == kotlin.Double::class.java -> bundle.putDouble(extra, value as Double)
-      clazz == kotlin.Int::class.java -> bundle.putInt(extra, value as Int)
-      clazz == kotlin.Long::class.java -> bundle.putLong(extra, value as Long)
-      clazz == kotlin.String::class.java -> bundle.putString(extra, value as String)
-      clazz == kotlin.CharSequence::class.java -> bundle.putCharSequence(extra, value as CharSequence)
-      clazz == kotlin.Float::class.java -> bundle.putFloat(extra, value as Float)
-      clazz == kotlin.Enum::class.java -> bundle.putEnum(extra, value as Enum<*>)
-
-      clazz == java.lang.Boolean::class.java -> bundle.putBoolean(extra, value as Boolean)
-      clazz == java.lang.Double::class.java -> bundle.putDouble(extra, value as Double)
-      clazz == java.lang.Integer::class.java -> bundle.putInt(extra, value as Int)
-      clazz == java.lang.Long::class.java -> bundle.putLong(extra, value as Long)
-      clazz == java.lang.String::class.java -> bundle.putString(extra, value as String)
-      clazz == java.lang.CharSequence::class.java -> bundle.putCharSequence(extra, value as CharSequence)
-      clazz == java.lang.Float::class.java -> bundle.putFloat(extra, value as Float)
-      clazz == java.lang.Enum::class.java -> bundle.putEnum(extra, value as Enum<*>)
-
-      clazz == Parcelable::class.java -> bundle.putParcelable(extra, value as Parcelable)
-      clazz == Serializable::class.java -> bundle.putSerializable(extra, value as Serializable)
-
-      kotlin.Enum::class.java.isAssignableFrom(clazz) -> bundle.putEnum(extra, value as Enum<*>)
-      java.lang.Enum::class.java.isAssignableFrom(clazz) -> bundle.putEnum(extra, value as Enum<*>)
-
-      else -> throw UnsupportedOperationException("Type ${clazz.name} is not supported")
-    }
-  }
-
-  private fun onGetArgumentsFromSource(source: Any): Bundle? {
+  private fun getArgumentsFromSource(source: Any): Bundle? {
     return when {
       source is ArgumentsAware -> source.arguments
       SupportHelper.isFragment(source) -> SupportFragmentHelper.getArguments(source)
@@ -175,7 +116,7 @@ private class ArgumentsVarDelegate<T, V>(
     }
   }
 
-  private fun onSetArgumentsToSource(source: Any, bundle: Bundle) {
+  private fun setArgumentsToSource(source: Any, bundle: Bundle) {
     when {
       source is ArgumentsAware -> source.arguments = bundle
       SupportHelper.isFragment(source) -> SupportFragmentHelper.setArguments(source, bundle)
@@ -193,18 +134,140 @@ private fun Bundle.replaceExtras(extras: Bundle) {
   putAll(extras)
 }
 
-private fun <E : Any?> Bundle.getEnum(name: String, clazz: Class<E>): E {
-  val string = getString(name, "")
-
-  if (!TextUtils.isEmpty(string)) {
-    return clazz.enumConstants.firstOrNull {
-      string == (it as Enum<*>).name()
-    } ?: clazz.enumConstants[0]
+@Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN", "UNCHECKED_CAST", "CAST_NEVER_SUCCEEDS")
+private fun <T> createTypeAdapterFor(clazz: Class<T>): Adapter<T> {
+  fun typed(desiredClass: Class<*>): Boolean {
+    return desiredClass.isAssignableFrom(clazz)
   }
+  
+  return when {
+    typed(kotlin.Boolean::class.java) -> BooleanAdapter
+    typed(kotlin.Double::class.java) -> DoubleAdapter
+    typed(kotlin.Int::class.java) -> IntAdapter
+    typed(kotlin.Long::class.java) -> LongAdapter
+    typed(kotlin.String::class.java) -> StringAdapter
+    typed(kotlin.CharSequence::class.java) -> CharSequenceAdapter
+    typed(kotlin.Float::class.java) -> FloatAdapter
+    typed(kotlin.Enum::class.java) -> EnumAdapter(clazz as Class<Enum<*>>)
 
-  return clazz.enumConstants[0]
+    typed(java.lang.Boolean::class.java) -> BooleanAdapter
+    typed(java.lang.Double::class.java) -> DoubleAdapter
+    typed(java.lang.Integer::class.java) -> IntAdapter
+    typed(java.lang.Long::class.java) -> LongAdapter
+    typed(java.lang.String::class.java) -> StringAdapter
+    typed(java.lang.CharSequence::class.java) -> CharSequenceAdapter
+    typed(java.lang.Float::class.java) -> FloatAdapter
+
+    typed(Parcelable::class.java) -> ParcelableAdapter
+    typed(Serializable::class.java) -> SerializableAdapter
+
+    else -> throw UnsupportedOperationException("Unable to create a type Adapter for \"${clazz.name}\"")
+  } as Adapter<T>
 }
 
-private fun Bundle.putEnum(name: String, value: Enum<*>) {
-  putString(name, value.name())
+private interface Adapter<T> {
+  public operator fun set(bundle: Bundle, name: String, value: T): Unit
+  public operator fun get(bundle: Bundle, name: String): T
+}
+
+private object BooleanAdapter : Adapter<Boolean> {
+  override operator fun set(bundle: Bundle, name: String, value: Boolean) {
+    bundle.putBoolean(name, value)
+  }
+
+  override operator fun get(bundle: Bundle, name: String): Boolean {
+    return bundle.getBoolean(name)
+  }
+}
+
+private object DoubleAdapter : Adapter<Double> {
+  override operator fun set(bundle: Bundle, name: String, value: Double) {
+    bundle.putDouble(name, value)
+  }
+
+  override operator fun get(bundle: Bundle, name: String): Double {
+    return bundle.getDouble(name)
+  }
+}
+
+private object IntAdapter : Adapter<Int> {
+  override operator fun set(bundle: Bundle, name: String, value: Int) {
+    bundle.putInt(name, value)
+  }
+
+  override operator fun get(bundle: Bundle, name: String): Int {
+    return bundle.getInt(name)
+  }
+}
+
+private object LongAdapter : Adapter<Long> {
+  override operator fun set(bundle: Bundle, name: String, value: Long) {
+    bundle.putLong(name, value)
+  }
+
+  override operator fun get(bundle: Bundle, name: String): Long {
+    return bundle.getLong(name)
+  }
+}
+
+private object StringAdapter : Adapter<String> {
+  override operator fun set(bundle: Bundle, name: String, value: String) {
+    bundle.putString(name, value)
+  }
+
+  override operator fun get(bundle: Bundle, name: String): String {
+    return bundle.getString(name)
+  }
+}
+
+private object CharSequenceAdapter : Adapter<CharSequence> {
+  override operator fun set(bundle: Bundle, name: String, value: CharSequence) {
+    bundle.putCharSequence(name, value)
+  }
+
+  override operator fun get(bundle: Bundle, name: String): CharSequence {
+    return bundle.getCharSequence(name)
+  }
+}
+
+private object FloatAdapter : Adapter<Float> {
+  override operator fun set(bundle: Bundle, name: String, value: Float) {
+    bundle.putFloat(name, value)
+  }
+
+  override operator fun get(bundle: Bundle, name: String): Float {
+    return bundle.getFloat(name)
+  }
+}
+
+private class EnumAdapter(val clazz: Class<Enum<*>>) : Adapter<Enum<*>> {
+  override operator fun set(bundle: Bundle, name: String, value: Enum<*>) {
+    bundle.putString(name, value.name())
+  }
+
+  override operator fun get(bundle: Bundle, name: String): Enum<*> {
+    return clazz.enumConstants.firstOrNull {
+      it.name() == bundle.getString(name)
+    } ?: throw IllegalArgumentException("\"$name\" is not a constant in \"${clazz.name}\"")
+  }
+}
+
+private object ParcelableAdapter : Adapter<Parcelable> {
+  override operator fun set(bundle: Bundle, name: String, value: Parcelable) {
+    bundle.putParcelable(name, value)
+  }
+
+  override operator fun get(bundle: Bundle, name: String): Parcelable {
+    return bundle.getParcelable(name)
+  }
+}
+
+private object SerializableAdapter : Adapter<Serializable> {
+  override operator fun set(bundle: Bundle, name: String, value: Serializable) {
+    bundle.putSerializable(name, value)
+  }
+
+  override operator fun get(bundle: Bundle, name: String): Serializable {
+    return bundle.getSerializable(name)
+  }
 }
